59528636eae279c28c72b552b41180c1b71a973d
wuhibe <wuhibetamire710@gmail.com>
2022-04-05 02:25:30 +0300

    budget

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,9 +1,11 @@
 import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
-  return {
-    ...(getBudgetObject(income, gdp, capita)),
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
     getIncomeInDollars: () => `$${income}`,
     getIncomeInEuros: () => `${income} euros`,
   };
+  return fullBudget;
 }
