b459e2d6355d4aa11f4c881ad79d0da61416a5e1
wuhibe <wuhibetamire710@gmail.com>
2022-04-05 01:49:41 +0300

    template

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (let idx of array) {
-    idx = appendString + idx;
+    idx = `${appendString}${idx}`;
   }
   return array;
 }
