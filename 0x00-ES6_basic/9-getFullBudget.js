e52fa190090205cf00077b91f1df15c65f06ac2c
wuhibe <wuhibetamire710@gmail.com>
2022-04-05 00:58:49 +0300

    no empty obj

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -2,11 +2,9 @@ import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
+  return {
     ...budget,
     getIncomeInDollars: () => `$${income}`,
     getIncomeInEuros: () => `${income} euros`,
   };
-
-  return fullBudget;
 }
