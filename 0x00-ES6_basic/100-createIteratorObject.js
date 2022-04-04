8dbe6cf2fbc69e2155bccbb6eb1509b5c2f96ffd
wuhibe <wuhibetamire710@gmail.com>
2022-04-05 02:29:02 +0300

    array of emps

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,11 @@
 export default function createIteratorObject(report) {
-  return [...(report.allEmployees.values())];
+  const array = [];
+  // eslint-disable-next-line
+    for (const lvl1 of Object.keys(report.allEmployees)) {
+    // eslint-disable-next-line
+        for (const lvl2 of report.allEmployees[lvl1]) {
+      array.push(lvl2);
+    }
+  }
+  return array;
 }
