10c7a2e3b5cccb153e8f3f03c4e2e7bdd2e7b23b
wuhibe <wuhibetamire710@gmail.com>
2022-04-05 20:35:51 +0300

    ignore first line

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,4 +1,5 @@
-import getBudgetObject from './7-getBudgetObject';
+// eslint-disable-next-line
+  import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
