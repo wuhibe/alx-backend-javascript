e5de4a502badb85ae757898a674a456e5118e6d9
wuhibe <wuhibetamire710@gmail.com>
2022-04-07 11:05:58 +0300

    morning commit

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,20 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  // getters
+  get code() { return this._code; }
+
+  get name() { return this._name; }
+
+  // setters
+  set code(code) { this._code = code; }
+
+  set name(name) { this._name = name; }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
