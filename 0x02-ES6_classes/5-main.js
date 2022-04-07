90cd7cfc8169b4eef4991ce8f16f56f51a67d3c8
wuhibe <wuhibetamire710@gmail.com>
2022-04-07 21:05:22 +0300

    Abstract class

---
+++ 0x02-ES6_classes/5-main.js
@@ -0,0 +1,21 @@
+import Building from './5-building.js';
+
+const b = new Building(100);
+console.log(b);
+
+class TestBuilding extends Building {}
+class NewBuilding extends Building {
+    evacuationWarningMessage() {
+        console.log('Abracadabra');
+    }
+}
+try {
+    new NewBuilding(123)
+} catch (err) {
+    console.log(err);
+}
+try {
+    new TestBuilding(200)
+} catch (err) {
+    console.log(err);
+}
