bb59aeef784c3a5ac0eead842d8a7dcce150f567
wuhibe <wuhibetamire710@gmail.com>
2022-04-07 20:14:19 +0300

    assert type

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,9 @@
+const assert = require('assert');
+
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    assert(typeof (name) === 'string', TypeError('Name must be a string'));
+    assert(typeof (length) === 'number', TypeError('Length must be a number'));
     this._name = name;
     this._length = length;
     this._students = students;
@@ -11,6 +15,7 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
+    assert(typeof (name) === 'string', TypeError('Name must be a string'));
     this._name = name;
   }
 
@@ -20,6 +25,7 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
+    assert(typeof (length) === 'number', TypeError('Length must be a number'));
     this._length = length;
   }
 
