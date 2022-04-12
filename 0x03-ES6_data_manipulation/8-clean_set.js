776e6e6e0dd2f975cab24510982ab0d92bb6fb29
wuhibe <wuhibetamire710@gmail.com>
2022-04-13 00:05:04 +0300

    empty arr element check

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, str) {
   if (str === '' || !(typeof str === 'string')) return '';
   const arr = [];
   for (const val of set) {
-    if (val.startsWith(str)) arr.push(val.slice(str.length));
+    if (val && val.startsWith(str)) arr.push(val.slice(str.length));
   }
   return arr.join('-');
 }
