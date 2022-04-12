3858215be308be10dfea876cf6b32c89d71fdf26
wuhibe <wuhibetamire710@gmail.com>
2022-04-13 00:14:12 +0300

    count

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,8 +3,7 @@ let count = 1;
 export function queryAPI(endPoint) {
   weakMap.set(endPoint, count);
   count += 1;
-  const idx = count - 1;
-  if (idx - 1 >= 5) {
+  if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
 }
