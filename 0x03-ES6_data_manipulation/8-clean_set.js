4a6ba0af45d981c1fee7bca7ce1f5d819ae09d71
wuhibe <wuhibetamire710@gmail.com>
2022-04-12 22:51:35 +0300

    add all elements

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,12 @@
 export default function cleanSet(set, str) {
+  if (str === '') return '';
   const iterable = set.values();
   const arr = [];
   for (let val of iterable) {
     if (val.includes(str, 0)) {
       val = val.replace(str, '');
       arr.push(val);
-    }
+    } else arr.push(val);
   }
   return arr.join('-');
 }
