73e13b010a21e112ba95abf389145a0256ed32b3
wuhibe <wuhibetamire710@gmail.com>
2022-04-12 22:53:30 +0300

    new Map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const myMap = new Map();
+  myMap.set('Apples', 10)
+    .set('Tomatoes', 10)
+    .set('Pasta', 1)
+    .set('Rice', 1)
+    .set('Banana', 5);
+  return myMap;
+}
