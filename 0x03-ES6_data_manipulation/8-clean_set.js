3ddc34ba875f94161bab123416cdf83136f47f01
wuhibe <wuhibetamire710@gmail.com>
2022-04-12 23:52:52 +0300

    startsWith

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,9 @@
 export default function cleanSet(set, str) {
-  if (str === '') return '';
-  const iterable = set.values();
-  const arr = [];
-  for (let val of iterable) {
-    if (val.includes(str, 0)) {
-      val = val.replace(str, '');
-      arr.push(val);
-    } else arr.push(val);
-  }
-  return arr.join('-');
+    if (str === '') return '';
+    const iterable = set.values();
+    const arr = [];
+    for (const val of iterable) {
+        if (val.startsWith(str)) arr.push(val.replace(str, ''));
+    }
+    return arr.join('-');
 }
