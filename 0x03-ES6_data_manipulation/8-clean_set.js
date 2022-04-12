657396395f3a2fc075682d72b9b66a1793cf6848
wuhibe <wuhibetamire710@gmail.com>
2022-04-12 23:54:00 +0300

    eslint

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, str) {
-    if (str === '') return '';
-    const iterable = set.values();
-    const arr = [];
-    for (const val of iterable) {
-        if (val.startsWith(str)) arr.push(val.replace(str, ''));
-    }
-    return arr.join('-');
+  if (str === '') return '';
+  const iterable = set.values();
+  const arr = [];
+  for (const val of iterable) {
+    if (val.startsWith(str)) arr.push(val.replace(str, ''));
+  }
+  return arr.join('-');
 }
