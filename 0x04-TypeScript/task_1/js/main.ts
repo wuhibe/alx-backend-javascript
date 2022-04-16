2836d6af3a3a6fec71a0eacdd8a090910cc0ae75
wuhibe <wuhibetamire710@gmail.com>
2022-04-16 03:02:07 +0300

    task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -27,3 +27,38 @@ const dir: Directors = {
   numberOfReports: 17
 };
 console.log(dir);
+
+function printTeacher(firstName: string, lastName: string) : string {
+  return `${firstName[0]}. ${lastName}}`;
+}
+interface printTeacherFunction{
+  printTeacher(firstName: string, lastName: string): string;
+}
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentClassConstructorInterface {
+  new(firstName: string, lastName: string): StudentClassInterface; 
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+      return 'Currently working';
+  }
+
+  displayName(): string {
+      return this.firstName;
+  }
+}
+
