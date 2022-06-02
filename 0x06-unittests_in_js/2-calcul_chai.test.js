72cbda2c01ad09b05ceb1610f1ccf2e8d4cc4a61
wuhibe <wuhibetamire710@gmail.com>
2022-06-03 02:19:30 +0300

    describe tests

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,8 +1,24 @@
-const { expect } = require('chai').expect;
+const { expect } = require('chai');
 const calcul = require('./2-calcul_chai');
 
-expect(calcul('SUM', 2, 5)).to.equal(7);
-expect(calcul('SUM', 2, 5)).to.be.a('number');
-expect(calcul('SUBTRACT', 22.1, 7.6)).to.equal(14);
-expect(calcul('DIVIDE', 2.2, 5)).to.equal(0.4);
-expect(calcul('DIVIDE', 2.2, 0)).to.equal('Error');
+describe('test sum with chai', () => {
+  it('test result', () => {
+    expect(calcul('SUM', 2, 5)).to.equal(7);
+  });
+  it('test type', () => {
+    expect(calcul('SUM', 2, 5)).to.be.a('number');
+  });
+});
+describe('test subtract with chai', () => {
+  it('test result', () => {
+    expect(calcul('SUBTRACT', 22.1, 7.6)).to.equal(14);
+  });
+});
+describe('test divide with chai', () => {
+  it('test result', () => {
+    expect(calcul('DIVIDE', 2.2, 5)).to.equal(0.4);
+  });
+  it('test result', () => {
+    expect(calcul('DIVIDE', 2.2, 0)).to.equal('Error');
+  });
+});
