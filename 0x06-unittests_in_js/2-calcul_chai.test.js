96cd092c9e86318754655af39adf6fd121ef7a9d
wuhibe <wuhibetamire710@gmail.com>
2022-06-03 02:12:44 +0300

    .expect

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,7 +1,8 @@
-const expect = require('chai');
+const { expect } = require('chai').expect;
 const calcul = require('./2-calcul_chai');
 
 expect(calcul('SUM', 2, 5)).to.equal(7);
+expect(calcul('SUM', 2, 5)).to.be.a('number');
 expect(calcul('SUBTRACT', 22.1, 7.6)).to.equal(14);
 expect(calcul('DIVIDE', 2.2, 5)).to.equal(0.4);
 expect(calcul('DIVIDE', 2.2, 0)).to.equal('Error');
