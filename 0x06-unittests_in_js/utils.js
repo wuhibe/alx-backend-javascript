c2dd6f3b5d5f11e8adc340e0a5593ea4b8f6be79
wuhibe <wuhibetamire710@gmail.com>
2022-06-03 03:02:14 +0300

    utils

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,11 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    if (type === 'SUM') return (Math.round(a) + Math.round(b));
+    if (type === 'SUBTRACT') return (Math.round(a) - Math.round(b));
+    if (type === 'DIVIDE') {
+      if (Math.round(b) !== 0) return (Math.round(a) / Math.round(b));
+    }
+    return 'Error';
+  },
+};
+module.exports = Utils;
